95913e0f30b04cf1c4a1b7bab02f6283881db7d4
Pavan Kunchala <pavankuchalapk@gmail.com>
2019-09-25 15:09:46 +0530

    little changes

--- Assets/Scripts/Enemies/RoboEnemy.cs
+++ Assets/Scripts/Enemies/RoboEnemy.cs
@@ -7,8 +7,12 @@ public class RoboEnemy : Enemy
     // Start is called before the first frame update
     void Start()
     {
+        base.Init();
+    }
+    private void Update()
+    {
         
     }
-    
+
 
 }
