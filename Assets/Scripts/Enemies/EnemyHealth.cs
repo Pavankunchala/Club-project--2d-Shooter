04f4ee334e4ed9f3d3eca30aaebfa4aa16a4988f
Pavan Kunchala <pavankuchalapk@gmail.com>
2019-09-25 10:27:13 +0530

    Initial commit

---
+++ Assets/Scripts/Enemies/EnemyHealth.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class EnemyHealth : MonoBehaviour {
+
+    public float fullHealth;
+
+    public float currentHealth;
+
+    private AudioSource eneAS;
+    [SerializeField]
+    private AudioClip enemClip;
+
+    [SerializeField]
+    private GameObject exploEffect;
+
+    [SerializeField]
+    private Slider enemyHealthSlider;
+
+
+
+	// Use this for initialization
+	void Start () {
+        eneAS = GetComponent<AudioSource>();
+        currentHealth = fullHealth;
+        enemyHealthSlider.maxValue = fullHealth;
+        enemyHealthSlider.value = currentHealth;
+        
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void AddDamage(float damage)
+    {
+        enemyHealthSlider.gameObject.SetActive(true);
+        currentHealth -= damage;
+        
+        enemyHealthSlider.value = currentHealth;
+        eneAS.PlayOneShot(enemClip);
+
+        if(currentHealth<= 0)
+        {
+            
+            Destroy(gameObject);
+            Instantiate(exploEffect, transform.position, Quaternion.identity);
+        }
+    }
+
+    
+
+
+}
